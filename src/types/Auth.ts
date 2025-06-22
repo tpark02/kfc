50b4ead7bf39ec4d1838b4ba22e4cfc3502914c9
tpark02 <tpark3546@gmail.com>
2025-06-22 22:00:54 +0900

    deploy env added

--- src/types/Auth.ts
+++ src/types/Auth.ts
@@ -12,15 +12,17 @@ export const getProtectedData = async () => {
   const token = localStorage.getItem("token");
   console.log("🔐 Token:", token);
 
-  const response = await fetch("http://localhost:8080/api/protected", {
-    headers: {
-      Authorization: `Bearer ${token}`, // ✅ 꼭 있어야 해
-      "Content-Type": "application/json",
-    },
-  });
+  const response = await fetch(
+    import.meta.env.VITE_API_URL + "/api/protected",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`, // ✅ 꼭 있어야 해
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
   if (!response.ok) throw new Error("인증 실패");
 
   return await response.json();
 };
-
